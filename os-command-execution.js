78eb0bd89412d306a514f0469cb4a50a509d2ab2
Franciny Rojas <120403457+fasrm@users.noreply.github.com>
2022-12-12 14:29:11 -0300

    Add files via upload

---
+++ os-command-execution.js
@@ -0,0 +1,13 @@
+var http = require("http);
+var url = require("url");
+var exe = require('child_process');
+http.createServer(function(request, response)
+{
+	var parsedUrl = url.parse(request.url, true);
+	response.writeHead(200, {"Content-Type":"text/html"});
+	exe.exec('ping -c 2' + parsedUrl.query.píng, function (err, data)
+	{
+	response.write("Hello "+ data);
+	response.end();
+	});
+}).listen(8888);
